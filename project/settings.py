63bb8a3cfb19df0bf39f95385c127a60f1e1939f
Mahesh.N.D <maheshndevraj@gmail.com>
2023-06-17 09:16:20 +0530

    commit3

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-dndvm_p=^dxcff8lb3*rqi65dsj!p62zk*8fz8va9qx8y-7a-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.row.sh']
 
 
 # Application definition
@@ -124,6 +124,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles','app/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
