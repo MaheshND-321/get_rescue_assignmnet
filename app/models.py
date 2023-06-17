6f19e2292b444ea3c4524be294d04cdf5957d4c1
Mahesh.N.D <maheshndevraj@gmail.com>
2023-06-17 07:16:02 +0530

    commit2

--- app/models.py
+++ app/models.py
@@ -1,34 +1,47 @@
 from django.db import models 
 from django.contrib.auth.models import User
 
+choice=(
+    ('1','inque'),
+    ('2','assigned'),
+    ('3','dispatched'),
+)
+
+
 class Issue(models.Model):
     issue_id = models.AutoField(primary_key=True)
-    user_id = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    problem = models.TextField()
-    time = models.CharField(max_length=100)
-    status = models.CharField(max_length=20)
+    user_id = models.CharField(max_length=100,default=0)
+    user_name = models.CharField(max_length=100,default=0)
+    location = models.CharField(max_length=100,default=0)
+    problem = models.TextField(default=0)
+    time = models.CharField(max_length=100,default=0)
+    status = models.CharField(max_length=100,choices=choice,null=False,default='inque')
 
 
 class Agent1(models.Model):
     agent_id = models.AutoField(primary_key=True)
-    queue = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=100,default=0)
+    queue = models.CharField(max_length=100,choices=choice,null=False,default='inque')
 
 class Agent2(models.Model):
     agent_id = models.AutoField(primary_key=True)
-    queue = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=100,default=0)
+    queue = models.CharField(max_length=100,choices=choice,null=False,default='inque')
 
 class Agent3(models.Model):
     agent_id = models.AutoField(primary_key=True)
-    queue = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=100,default=0)
+    queue = models.CharField(max_length=100,choices=choice,null=False,default='inque')
 
 class Agent4(models.Model):
     agent_id = models.AutoField(primary_key=True)
-    queue = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=100,default=0)
+    queue = models.CharField(max_length=100,choices=choice,null=False,default='inque')
 
 class Agent5(models.Model):
     agent_id = models.AutoField(primary_key=True)
-    queue = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=100,default=0)
+    queue = models.CharField(max_length=100,choices=choice,null=False,default='inque')
 
 
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Issue, Agent1,Agent2,Agent3,Agent4,Agent5, Mechanic
 import random
+from django.contrib import messages
 
 
 i=0
@@ -19,28 +20,31 @@ def index(request):
         usr_id="usr_id"+str(i)
         status='Inque'
         j=i%5
-        issue_table = Issue(issue_id=iss_id,user_id=usr_id,location=location,problem=problem,time=time,status=status)
+    
+        issue_table = Issue(issue_id=iss_id,user_id=usr_id,user_name=user_name,location=location,problem=problem,time=time,status=status)
         issue_table.save()
         match  j:
             case 1:
-                agent_table = Agent1(queue=usr_id)
+                agent_table = Agent1(user_id=usr_id,queue=status)
             case 2:
-                agent_table = Agent2(queue=usr_id)
+                agent_table = Agent2(user_id=usr_id,queue=status)
             case 3:
-                agent_table = Agent3(queue=usr_id)
+                agent_table = Agent3(user_id=usr_id,queue=status)
             case 4:
-                agent_table = Agent4(queue=usr_id)
+                agent_table = Agent4(user_id=usr_id,queue=status)
             case 5:
-                agent_table = Agent5(queue=usr_id)
+                agent_table = Agent5(user_id=usr_id,queue=status)
             
         agent_table.save()
-        print(iss_id)
-        print(usr_id)
-        print(location)
-        print(problem)
-        print(time)
-        print(status)
-        return HttpResponse("Submitted")
+        messages.success(request, 'Issue submitted successfully!')
+        # print(iss_id)
+        # print(usr_id)
+        # print(location)
+        # print(problem)
+        # print(time)
+        # print(status)
+        # return HttpResponse("Submitted")
+        return redirect('/')
     else:
         return render(request, 'index.html')
 
